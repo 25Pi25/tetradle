6cd5992fc9ae68a04c947672befe5a04a78d6448
25Pi25 <25pi25@mail.com>
2023-07-12 23:36:37 -0700

    its a crime i called the tetrio button PINK

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -6,7 +6,7 @@ export default function Help({ setShowHelp }: { setShowHelp: Dispatch<SetStateAc
             <p className='close-help' onClick={() => setShowHelp(false)}>X</p>
             <h1>HOW TO PLAY</h1>
             <p>1. Download the replay at the top of the browser to your device.</p>
-            <p>2. Open TETR.IO {"(or click the pink TETR.IO button)"} to watch the replay.</p>
+            <p>2. Open TETR.IO {"(or click the purple button)"} to watch the replay.</p>
             <p>3. Upload the replay to TETR.IO by dragging it into your browser.</p>
             <p>4. Enter your guess to both players' ratings!</p>
             <img className='example' src="/download-replay.gif" alt="how to download" />
